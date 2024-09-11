76857ee4c3b65f682df25ae0be398b0b760fd748
lbb97 <173640874+lbb97@users.noreply.github.com>
2024-09-11 17:07:53 +0200

    Added the code for the project to script.js. Game now works, stricktly via console.

---
+++ script/script.js
@@ -0,0 +1,73 @@
+function getComputerChoice() {
+    let theChoice = Math.floor(Math.random()*3+1)
+
+    if (theChoice === 1) {
+        return "Rock"
+    }
+    else if (theChoice === 2) {
+        return "Paper"
+    }
+    else return "Scissors"
+};
+
+function getHumanChoice() {
+    let humanChoice = parseInt(prompt("Num 1 for rock, Num 2 for paper and Num 3 for scissors. Your choice?", ""));
+
+    if (humanChoice === 1) {
+        return "Rock"
+    }
+    else if (humanChoice === 2) {
+        return "Paper"
+    }
+    else if (humanChoice === 3) {
+        return "Scissors"
+    }
+    else return "Choose a valid number. Try again."
+};
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame() {
+
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    function playRound() {
+        if (humanChoice === computerChoice) {
+            return "tie";
+        } else if (
+        (humanChoice === "Rock" && computerChoice === "Scissors") ||
+        (humanChoice === "Scissors" && computerChoice === "Paper") ||
+        (humanChoice === "Paper" && computerChoice === "Rock")) {
+            return "human";
+        } else return "computer";
+    };
+
+    console.log("The computer chose " + computerChoice);
+    console.log("You chose " + humanChoice);
+
+    let result = playRound();
+
+    if (result === "tie") {
+        humanScore += 1;
+        computerScore += 1;
+    } else if (result === "human") {
+        humanScore += 1;
+    } else computerScore +=1;
+
+    console.log("*Current score* \nComp-ew-tah: " + computerScore + "." + "\nHooman: " + humanScore + ".");
+
+};
+
+for (let i = 0; i < 5; i++) {
+    playGame();
+}
+
+if (humanScore >= computerScore) {
+    console.log("GAME OVER!");
+    console.log('Hooman wins')
+} else if (computerScore >= humanScore) {
+    console.log("GAME OVER!");
+    console.log('Comp-ew-tah wins')
+} else "It's chai";
